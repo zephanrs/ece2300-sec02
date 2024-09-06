0a0ffd2af85ca5a968f6fc40b169d7014ae1fe5c
cb535 <cb535@cornell.edu>
2024-09-06 12:53:40 -0400

    ready for section

--- PairTripleDetector.v
+++ PairTripleDetector.v
@@ -10,14 +10,10 @@ module PairTripleDetector
   output wire out
 );
 
-  wire w;
-  wire x;
-  wire y;
-
-  or ( w,   in0, in1 );
-  and( x,   in0, in1 );
-  and( y,   w,   in2 );
-  or ( out, y,   x   );
+  // '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+  // Discussion Section Task
+  // '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+  // Implement a pair/triple detector using explicit gate-level modeling.
 
 endmodule
 
